5236b00468804da2ad1fc398d4d59e3325010fcc
VernalEquinox <pieroapruzzese@gmail.com>
2021-02-11 22:55:50 +0100

    ko

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
-import React,{Component} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-import {BrowserRouter as Router} from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom";
 
-import SearchBar from './components/SearchBar'
+import SearchBar from "./components/SearchBar";
 
-/* interface State  {
-  data: any
-} */
+interface State {
+  songs: any;
+}
 
 /* searchResultHandler(dataSearch) => {
   this.setState({data: dataSearch})
 }
  */
-class App extends Component{
+class App extends Component<State> {
+  state = { songData: [] };
+
+  songsDataHandler = (songs) => {
+    this.setState({ songsData: songs });
+  };
   render() {
-    return (
-      <Router>
-        <div className="App">
-          <SearchBar artist="radiohead" />
-          
-      
-        </div>
-      </Router>
-    );
+    if (this.state.songData)
+      return (
+        <Router>
+          <div className="App">
+            <SearchBar songsDataHandler={this.songsDataHandler} />
+          </div>
+        </Router>
+      );
   }
 }
 
